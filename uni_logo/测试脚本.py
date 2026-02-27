868e5eda2953095510f8196d91a0b4fd50f4c6d4
Stardust_Lu路星沉 <134242223+StardustLu1@users.noreply.github.com>
2026-02-27 23:29:31 +0800

    Update 测试脚本.py

--- uni_logo/测试脚本.py
+++ uni_logo/测试脚本.py
@@ -9,9 +9,8 @@ from tencentcloud.common.exception.tencent_cloud_sdk_exception import TencentClo
 from tencentcloud.cvm.v20170312 import cvm_client, models
 try:
     # 实例化一个认证对象，入参需要传入腾讯云账户 SecretId 和 SecretKey，此处还需注意密钥对的保密
-    # 代码泄露可能会导致 SecretId 和 SecretKey 泄露，并威胁账号下所有资源的安全性
     # 以下代码示例仅供参考，建议采用更安全的方式来使用密钥
-    # 请参见：https://cloud.tencent.com/document/product/1278/85305
+    # 请参见：https://cloud.tencent.com/document/product
     # 密钥可前往官网控制台 https://console.cloud.tencent.com/cam/capi 进行获取
     cred = credential.Credential("SecretId", "SecretKey")
     # 使用临时密钥示例
@@ -39,4 +38,5 @@ try:
     print(resp.to_json_string())
 
 except TencentCloudSDKException as err:
+
     print(err)
